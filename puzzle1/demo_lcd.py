7ad3d6864b3faaba64ce3891a174a3a99795fbef
davidF2801 <67702016+davidF2801@users.noreply.github.com>
2021-10-04 19:54:45 +0200

    Update demo_lcd.py

--- puzzle1/demo_lcd.py
+++ puzzle1/demo_lcd.py
@@ -1,16 +1,10 @@
 #! /usr/bin/env python
-
-# Simple string program. Writes and updates strings.
 # Demo program for the I2C 16x2 Display from Ryanteck.uk
 # Created by Matthew Timmons-Brown for The Raspberry Pi Guy YouTube channel
 
 # Import necessary libraries for communication and display use
 import drivers #Importem la carpeta drivers
-
-# Load the driver and set it to "display"
 display = drivers.Lcd()
-
-# Main body of code
 #La següent funció la usaré per a que el string entrat es col·loqui a les files corresponents per ordre, sense fer això, 
 #quan un string ocupa més de 20 caracters, es coloca en l'order incorrecte
 def show_lines(message, long): 
@@ -28,19 +22,14 @@ def show_lines(message, long):
             display.lcd_display_string(message[20:40],2)
             display.lcd_display_string(message[40:60],3)
             display.lcd_display_string(message[60:long],4)
-           
-            
             
 if __name__ == "__main__":
     try:
-          
            statement = "Escribe lo que quieras imprimir: " #primer de tot treiem el missatge per pantalla de que s'escrigui el que es vol imprimir
            show_lines(statement,len(statement))
            message=input(statement)
            display.lcd_clear() #Un cop entrat el missatge es neteja la pantalla
            show_lines(message,len(message)) #Finalment es mostra el missatge per pantalla
-           
-           
     except KeyboardInterrupt: #Quan es prem ctrl + c, es neteja la pantalla i s'acaba l'execució del programa
         print("Cleaning up!")
         display.lcd_clear()
